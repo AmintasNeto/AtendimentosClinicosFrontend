db74b7a4efbdc53cf82859a7a0153306f8a14af4
AmintasNeto <netorubok14@gmail.com>
2025-07-15 09:20:31 -0300

    Atualiza componente de navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,15 @@
+import { Button, Nav } from "react-bootstrap";
 import { useAuth } from "../hooks/UseLogin";
 
 function Navbar() {
     const context = useAuth()
 
     return (
-    <nav>
-        <span>Bem vindo, {context?.user?.username}!</span>
+    <Nav className="d-flex" style={{justifyContent: "space-between", backgroundColor: "#f5f5f5", padding: 15}}>
+        <span style={{alignSelf: "center", fontWeight: "bold"}}>Bem vindo, {context?.user?.username}!</span>
 
-        <button className="btn btn-danger" onClick={() => context?.logout()}>Sair</button>
-    </nav>
+        <Button className="btn btn-danger" onClick={() => context?.logout()}>Sair</Button>
+    </Nav>
     );
 }
 
