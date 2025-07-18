38279cdd688f895e7e8530445270f460ee3f5f89
AmintasNeto <netorubok14@gmail.com>
2025-07-18 12:13:40 -0300

    Atualiza estilização na navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import { Button, Nav } from "react-bootstrap";
 import { useAuth } from "../hooks/UseLogin";
+import { FaSignOutAlt } from "react-icons/fa";
 
-function Navbar() {
+type NavProps = {
+    width: number;
+}
+
+function Navbar(props: NavProps) {
     const context = useAuth()
 
     return (
     <Nav className="d-flex" style={{justifyContent: "space-between", backgroundColor: "#f5f5f5", padding: 15}}>
         <span style={{alignSelf: "center", fontWeight: "bold"}}>Bem vindo, {context?.user?.username}!</span>
 
-        <Button className="btn btn-danger" onClick={() => context?.logout()}>Sair</Button>
+        <Button className="btn btn-danger button-shadow" onClick={() => context?.logout()}>{props.width >= 768 ? "Sair" : ""} <FaSignOutAlt /></Button>
     </Nav>
     );
 }
